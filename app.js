853ce31faeab1a646da213d1c21ec9a995f09b42
Matheus Gabriel Wozniak Passos <mgw356@hotmail.com>
2022-01-10 20:19:47 -0300

    most html done, start of js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+
+//event Listener
+todoButton.addEventListener('click', addTodo);
+
+//functions
+function addTodo(event){
+    //do not let the for to submit
+    event.preventDefault();
+    
+    //todo div
+    
+}
